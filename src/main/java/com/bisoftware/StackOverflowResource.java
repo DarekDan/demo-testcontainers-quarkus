ad39a3d9b75a5a6478c81dd7d1c606e3398237b3
Danielewski, Dariusz <dariusz.danielewski@fiserv.com>
2021-12-09 11:16:45 -0500

    Recreated tests ... again

--- src/main/java/com/bisoftware/StackOverflowResource.java
+++ src/main/java/com/bisoftware/StackOverflowResource.java
@@ -3,20 +3,22 @@ package com.bisoftware;
 import io.agroal.api.AgroalDataSource;
 import io.smallrye.mutiny.Uni;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
 
 @Path("/stackoverflow")
 public class StackOverflowResource {
 
     @Inject
     AgroalDataSource defaultDataSource;
+
+    @Inject
+    Logger logger;
 
     @GET
     @Path("randomUserName")
@@ -28,7 +30,9 @@ public class StackOverflowResource {
                 ResultSet resultSet = statement.executeQuery(
                     "SELECT TOP 1 DisplayName FROM [StackOverflow2010].[dbo].[Users] ORDER BY NEWID()");
                 resultSet.next();
-                return resultSet.getString(1);
+                final String randomName = resultSet.getString(1);
+                logger.info("Returning name: " + randomName);
+                return randomName;
             } catch (Exception e) {
                 return e.getMessage();
             }
