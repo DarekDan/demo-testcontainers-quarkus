2b772741ade3be3494e4a6d824e7908896cbe916
Danielewski, Dariusz <dariusz.danielewski@fiserv.com>
2021-12-09 11:08:05 -0500

    Recreated tests ... again

--- src/test/java/com/bisoftware/StackOverflowResourceTest.java
+++ src/test/java/com/bisoftware/StackOverflowResourceTest.java
@@ -2,6 +2,8 @@ package com.bisoftware;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.MSSQLServerContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -11,16 +13,19 @@ import org.testcontainers.utility.DockerImageName;
 @Testcontainers
 class StackOverflowResourceTest {
 
-    DockerImageName sqlImage = DockerImageName.parse("stackoverflow-2010-db").asCompatibleSubstituteFor("mcr.microsoft.com/mssql/server");
+    static DockerImageName sqlImage = DockerImageName.parse("stackoverflow-2010-db").asCompatibleSubstituteFor("mcr.microsoft.com/mssql/server");
 
     @Container
-    public MSSQLServerContainer mssqlserver = new MSSQLServerContainer(sqlImage)
+    static MSSQLServerContainer mssqlserver = new MSSQLServerContainer(sqlImage)
         .acceptLicense();
 
     @Test
-    public void someTestMethod() {
+    public void someTestMethod() throws SQLException {
         String url = mssqlserver.getJdbcUrl();
         System.out.println(url);
+        final ResultSet resultSet = mssqlserver.createConnection("").prepareStatement("SELECT GETDATE()").executeQuery();
+        resultSet.next();
+        System.out.println(resultSet.getObject(1));
         assertThat(url.isBlank()).isFalse();
     }
 
